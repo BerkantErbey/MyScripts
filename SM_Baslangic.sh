01c73e0acd6b46e820c1186668a61c90286b5255
Berkant <BerkantErbey@users.noreply.github.com>
2020-09-02 21:39:47 +0300

    Update SM_Baslangic.sh

--- SM_Baslangic.sh
+++ SM_Baslangic.sh
@@ -3,6 +3,6 @@ sudo add-apt-repository ppa:mc3man/mpv-tests
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update && sudo apt install valgrind openssh-server sublime-text mpv
+sudo apt update && sudo apt install openssh-server sublime-text mpv speedtest-cli
 sudo apt remove '^vlc.*'
 sudo apt autoremove
