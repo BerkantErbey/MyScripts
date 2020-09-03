a2ca32239f01ce63dafb59217624a6f4d61ab8b6
Berkant <BerkantErbey@users.noreply.github.com>
2020-09-03 17:02:17 +0300

    Update Ubuntu16.sh

--- Ubuntu16.sh
+++ Ubuntu16.sh
@@ -3,6 +3,6 @@ sudo add-apt-repository ppa:mc3man/mpv-tests
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update && sudo apt install openssh-server sublime-text mpv speedtest-cli putty htop vim
+sudo apt update && sudo apt install openssh-server sublime-text mpv speedtest-cli putty htop vim nmap
 sudo apt remove '^vlc.*'
 sudo apt autoremove
