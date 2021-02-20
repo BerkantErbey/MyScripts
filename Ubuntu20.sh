b8e2a33fa110c7074960163c0f52bb602d28b87c
Berkant <BerkantErbey@users.noreply.github.com>
2021-02-20 16:08:33 +0300

    Update Ubuntu20.sh

--- Ubuntu20.sh
+++ Ubuntu20.sh
@@ -9,6 +9,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install apt-transport-https
-sudo apt update && sudo apt install anydesk curl chromium-browser ffmpeg gedit-plugin-text-size git gnome-tweak-tool obs-studio youtube-dl htop mpv ncdu neofetch net-tools nmap openssh-server putty smartmontools speedtest-cli sublime-text tldr tree vim whois wireguard
+sudo apt update && sudo apt install anydesk curl ffmpeg gedit-plugin-text-size git gnome-tweak-tool obs-studio youtube-dl htop mpv ncdu neofetch net-tools nmap openssh-server net-tools sublime-text tldr tree vim whois wireguard
 sudo apt remove '^vlc.*' 
 sudo apt remove ryhthmbox
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
